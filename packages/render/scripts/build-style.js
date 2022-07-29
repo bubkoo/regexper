2643be6b3810c5d1cbbf23fbd9300c1d69fd0007
bubkoo <bubkoo.wy@gmail.com>
2022-07-29 17:28:22 +0800

    fix: build style

--- packages/render/scripts/build-style.js
+++ packages/render/scripts/build-style.js
@@ -6,18 +6,20 @@ const content = fs.readFileSync('src/style.css', 'utf8').trim()
 const code = `/* eslint-disable */
 // Auto generated file, do not modify it!
 
-export const style = \`${content}\`.trim()
+export const style = \`
+${content}
+\`.trim()
 `
 
 const file = 'src/style.ts'
-const old = fs.readFileSync(file, { encoding: 'utf8' })
-const format = (s) => s.replace(/[\n\s]/g, '')
+const old = fs.existsSync(file)
+  ? fs.readFileSync(file, { encoding: 'utf8' })
+  : null
 
-if (format(code) !== format(old)) {
+if (code !== old) {
   fs.writeFile(file, code, (err) => {
     if (err) {
       throw new Error(`An error occurred while generate style file: ${err} `)
     }
-    console.log(`Style file updated.`)
   })
 }
