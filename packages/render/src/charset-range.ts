17c5d72e813f0838090e680b511e0c4403930ae3
bubkoo <bubkoo.wy@gmail.com>
2025-04-07 11:29:46 +0800

    fix: handle escape node of charset-range type

--- packages/render/src/charset-range.ts
+++ packages/render/src/charset-range.ts
@@ -1,25 +1,34 @@
-import { Node } from './node'
+import { CharsetEscapeNode } from './charset-escape'
 import { LiteralNode } from './literal'
-import { spaceHorizontal } from './util'
+import { Node } from './node'
 import { Metadata } from './types'
+import { spaceHorizontal } from './util'
 
 /**
  * CharsetRange nodes are used for `[a-z]` regular expression syntax. The two
  * literal or escape nodes are rendered with a hyphen between them.
  */
 export class CharsetRangeNode extends Node {
-  public readonly first: LiteralNode
-  public readonly last: LiteralNode
+  public readonly first: LiteralNode | CharsetEscapeNode
+  public readonly last: LiteralNode | CharsetEscapeNode
 
   constructor(metadata: Metadata, parent: Node) {
     super(metadata, parent)
-    this.first = new LiteralNode(metadata.first, this)
-    this.last = new LiteralNode(metadata.last, this)
+    this.first = this.createTerminalNode(metadata.first as Metadata)
+    this.last = this.createTerminalNode(metadata.last as Metadata)
 
     // Report invalid expression when extents of the range are out of order.
-    if (this.first.order > this.last.order) {
-      throw new Error(`Range out of order in character class: ${this.text}`)
+    if (this.first instanceof LiteralNode && this.last instanceof LiteralNode) {
+      if (this.first.order > this.last.order) {
+        throw new Error(`Range out of order in character class: ${this.text}`)
+      }
     }
+  }
+
+  private createTerminalNode(metadata: Metadata) {
+    return metadata.type === 'charset-escape'
+      ? new CharsetEscapeNode(metadata, this)
+      : new LiteralNode(metadata, this)
   }
 
   async renderNode() {
